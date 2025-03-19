6d44d425628f5c626ac9026feedfd9252d1f57a1
GaoSoiBac <tomnguyenhieu2004@gmail.com>
2025-03-20 01:49:35 +0700

    Update Main.java

--- src/main/java/com/edu/duongdua/core/bandoan/Main.java
+++ src/main/java/com/edu/duongdua/core/bandoan/Main.java
@@ -4,7 +4,9 @@ import com.edu.duongdua.core.bandoan.Models.Connector;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -14,8 +16,9 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Quan ly mon an");
+        stage.setTitle("Quản lý món ăn");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
